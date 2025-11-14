5a65e1b500df99865e4ad9e79961e37071bc318c
Maheshi Wickramarathna <maheshiwickramarathna2001@gmail.com>
2025-11-15 00:48:35 +0530

    Recreated navigation bar without show in login page

--- frontend/ampora-frontend/src/App.jsx
+++ frontend/ampora-frontend/src/App.jsx
@@ -1,20 +1,39 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-// import Login from "./pages/Login";
+import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import TripPlanner from "./pages/TripPlanner";
+
+function Applaout(){
+    const location = useLocation();
+   const hideNavbar = location.pathname === "/login";
+
+
+  return (
+    <>
+        <div>
+          {!hideNavbar && <Navbar/>}
+        </div>
+        <div>
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/trip-planner" element={<TripPlanner />} />
+          </Routes>
+        </div>
+      
+    </>  
+  );
+
+
+}
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="/login" element={<Login />} /> */}
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/trip-planner" element={<TripPlanner />} />
-      </Routes>
+      <Applaout/>
     </Router>
   );
 }
